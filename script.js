f3b378953935dc7f7aa5c09105ba2c24a3933694
Tamal Chakraborty <ckbtamaldipnew@gmail.com>
2024-01-09 10:07:32 +0600

    Add javascript and project complete! Happy Happy Happy ;)

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello from the console");
+const display = document.querySelector('#display');
+
+const appendOnDisplay = (input) =>{
+    display.value += input;
+}
+
+const clearDisplay = () =>{
+    display.value = "";
+}
+
+const clearOne = () =>{
+    const value = display.value;
+    const valArray = value.split('');
+    valArray.splice(valArray.length-1,1).join();
+    display.value = valArray.join("");
+}
+
+const calculate = () =>{
+    try{
+        display.value = eval(display.value);
+    }catch(err){
+        display.value = "Error";
+    }
+}
